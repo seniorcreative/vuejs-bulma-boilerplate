0b8e0ea07e6d207a09bbe0feca7649f2b6ee1524
Steven Smith <steven_smith@worksafe.vic.gov.au>
2020-04-20 14:14:23 +1000

    Landing page styles

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container is-fluid">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Welcome</router-link>
     </div>
     <router-view/>
   </div>
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,8 +1,13 @@
 <template>
+  <!-- FYI , ensure this is wrapped in bulma outer container (see App component)  -->
   <section>
-    <h2>VueJS Bulma Boilerplate</h2>
-    <p>Why not scaffold with some <a href="https://docs.emmet.io/">emmet</a>&hellip;</p>
-    <p>Follow <a href="https://bulma.io">bulma</a> reference for sections, rows and columns that behave nicely and reponsively</p>
+    <div class="columns">
+      <div class="column  is-4  is-offset-4">
+          <h2 class="title  is-2 has-text-grey">VueJS Bulma Boilerplate</h2>
+          <p>Why not scaffold with some <a href="https://docs.emmet.io/">emmet</a>&hellip;</p>
+          <p>Follow <a href="https://bulma.io">bulma</a> reference for sections, rows and columns that behave nicely and reponsively</p>
+      </div>
+    </div>
   </section>
 </template>
 
